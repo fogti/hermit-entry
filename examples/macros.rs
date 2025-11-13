87d7c339c824645206d331c0daa50fb6c4aeb846
Ellen Εμιλία Άννα Zscheile <fogti+devel@ytrizja.de>
2025-11-13 19:35:46 +0100

    feat: add a compilation test to make sure the macros work

---
+++ examples/macros.rs
@@ -0,0 +1,7 @@
+//! This file tests if the macros defined by the hermit-entry crate,
+//! which affect linker .note entries, can actually be used.
+
+hermit_entry::define_abi_tag!();
+hermit_entry::define_entry_version!();
+
+fn main() {}
